b40f5b581ab23d9a559f638a175952a88bd92460
Kevin Skolglund <sohit1208@gmail.com>
2019-03-07 11:05:50 +0530

    updated flight schema

--- src/models/flight.js
+++ src/models/flight.js
@@ -4,8 +4,19 @@ const mongoose = require('mongoose'),
 
 const FlightSchema = new Schema({
   id  : { type : Number, required: false },
-  abbreviation   : { type : String, required: true, trim: true },
-  name    : { type : String, required: true, trim: true },// TODO : Change this condition. 
+  model    : { type : String, required: true, trim: true },
+  MSN    : { type : Number, required: true, trim: true },
+  harnessLen    : { type : Number, required: true, trim: true },
+  grossWt    : { type : Number, required: true, trim: true },
+  atmPress    : { type : Number, required: true, trim: true },
+  roomTemp    : { type : Number, required: true, trim: true },
+  airport    : { type : String, required: true, trim: true },
+  fuelCapLt    : { type : Number, required: true, trim: true },
+  fuelCapRt    : { type : Number, required: true, trim: true },
+  fuelQtyLt    : { type : Number, required: true, trim: true },
+  fuelQtyRt    : { type : Number, required: true, trim: true },
+  maxAlt    : { type : Number, required: true, trim: true },
+  fltNo    : { type : String, required: true, trim: true }
 });
 
 module.exports = mongoose.model('Flight', FlightSchema, 'flights');

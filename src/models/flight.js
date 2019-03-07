ee22937b88ac0c0086468b83e4ffd49eb2a65a04
Kevin Skolglund <sohit1208@gmail.com>
2019-03-07 11:26:48 +0530

    updated required fields for flight schema

--- src/models/flight.js
+++ src/models/flight.js
@@ -6,17 +6,17 @@ const FlightSchema = new Schema({
   id  : { type : Number, required: false },
   model    : { type : String, required: true, trim: true },
   MSN    : { type : Number, required: true, trim: true },
-  harnessLen    : { type : Number, required: true, trim: true },
-  grossWt    : { type : Number, required: true, trim: true },
-  atmPress    : { type : Number, required: true, trim: true },
-  roomTemp    : { type : Number, required: true, trim: true },
-  airport    : { type : String, required: true, trim: true },
-  fuelCapLt    : { type : Number, required: true, trim: true },
-  fuelCapRt    : { type : Number, required: true, trim: true },
-  fuelQtyLt    : { type : Number, required: true, trim: true },
-  fuelQtyRt    : { type : Number, required: true, trim: true },
-  maxAlt    : { type : Number, required: true, trim: true },
-  fltNo    : { type : String, required: true, trim: true }
+  harnessLen    : { type : Number, required: false, trim: true },
+  grossWt    : { type : Number, required: false, trim: true },
+  atmPress    : { type : Number, required: false, trim: true },
+  roomTemp    : { type : Number, required: false, trim: true },
+  airport    : { type : String, required: false, trim: true },
+  fuelCapLt    : { type : Number, required: false, trim: true },
+  fuelCapRt    : { type : Number, required: false, trim: true },
+  fuelQtyLt    : { type : Number, required: false, trim: true },
+  fuelQtyRt    : { type : Number, required: false, trim: true },
+  maxAlt    : { type : Number, required: false, trim: true },
+  fltNo    : { type : String, required: false, trim: true }
 });
 
 module.exports = mongoose.model('Flight', FlightSchema, 'flights');
